ad04b8838bec94c7de68ca9f799a08550e6254cc
Jonathan Garibaldi <123399633+Jonatohan@users.noreply.github.com>
2024-01-16 15:18:38 -0500

    adding style

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-  document.getElementById('project-1').addEventListener('click', function() {
+$(document).ready(function () {
+  $(".drone-button").addClass("animated bounce");
+  $(".travel-button").addClass("animated bounce");
+  $(".next-button").addClass("animated bounce");
+  $(".project-1").addClass("animated fadeIn");
+  $(".project-2").addClass("animated fadeIn");
+  $(".project-3").addClass("animated fadeIn");
+  $(".nombre").addClass("animated bounce");
+  
+});
+setInterval(function() {
+  $(".next-button").toggleClass("animated shake");
+}, 2000);
 
-     window.location.href = "https://codigo-buho.netlify.app/";
+ 
+ document.getElementById('project-1').addEventListener('click', function() {
+
+     window.location.href = "https://codigo-buho.vercel.app/";
  });
+
+ document.getElementById('project-3').addEventListener('click', function() {
+
+  window.location.href = "https://image-gallery-ten-tau.vercel.app/";
+});
